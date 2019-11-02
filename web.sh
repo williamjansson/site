e06db939847b9517724aaae2681bc74016fcdbb0
William <william.c.jansson@gmail.com>
2019-11-02 13:04:10 +0900

    rdirect

--- web.sh
+++ web.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/libexec/httpd -b -U nobody -Z /usr/pkg/etc/letsencrypt/live/www.jansson.studio/fullchain.pem /usr/pkg/etc/letsencrypt/live/www.jansson.studio/privkey.pem -z 'EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;' /var/www
+/usr/libexec/bozohttpd -bV -U nobody -Z /usr/pkg/etc/letsencrypt/live/jansson.studio/fullchain.pem /usr/pkg/etc/letsencrypt/live/jansson.studio/privkey.pem -z 'EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;' -v /var/vrootssl /var/www
+
+/usr/libexec/bozohttpd -b -U nobody -v /var/vroot /var/www
